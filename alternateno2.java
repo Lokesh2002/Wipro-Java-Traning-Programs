78236274ddd9025f53f2549052e1eb56274c319f
Lokeshwer <71865903+Lokesh2002@users.noreply.github.com>
2023-02-06 19:26:47 +0530

    Add files via upload

---
+++ alternateno2.java
@@ -0,0 +1,17 @@
+package wiproproject;
+
+public class alternateno2 {
+
+		public static void main(String[] args) {
+
+		for(int i=0; i<=100; i=i+2)
+
+		{
+
+		System.out.println(i);
+
+		}
+
+		}
+
+}
