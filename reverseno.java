6642677a7a233d5b0df68b955f2302df40188482
Lokeshwer <71865903+Lokesh2002@users.noreply.github.com>
2023-02-06 19:46:08 +0530

    Add files via upload

---
+++ reverseno.java
@@ -0,0 +1,19 @@
+package wiproproject;
+
+import java.util.*;
+public class reverseno {
+	static void PrintReverseOrder(int N)
+	{
+	 
+	    for (int i = N; i >= 0; i--)
+	        System.out.print( +i + " ");
+	}
+	public static void main(String[] args)
+	{
+	    int N = 100;
+	 
+	    PrintReverseOrder(N);
+	}
+	 
+
+}
