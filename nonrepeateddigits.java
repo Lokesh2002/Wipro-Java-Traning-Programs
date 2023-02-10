ac1e240c951ac9a1d6b9ed5fea29e209a55f85e5
Lokeshwer <71865903+Lokesh2002@users.noreply.github.com>
2023-02-10 11:14:54 +0530

    Add files via upload

---
+++ nonrepeateddigits.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class solution {
+
+
+ public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int no = scan.nextInt();
+        int count = 0;
+        for(int i=1;i<=no;i++)
+        {
+          String a = Integer.toString(i);
+          char[] b =a.toCharArray();
+
+          Arrays.sort(b);
+          String x = new String(b);
+      //    System.out.println("Sorted array:"+x);
+          int flag = 0;
+          if(a.length()>1)
+        {
+          for(int j=1;j<a.length();j++)
+          {
+            //System.out.println(b[j-1]+"=="+b[j]);
+            if( (b[j-1]==b[j]))
+            {
+              flag =1;
+            //  System.out.println("Has Repeated Numbers!");
+              break;
+            }
+          }
+        }
+          if(flag == 0)
+          {
+            count++;
+          //  System.out.println("count:"+count+" --> "+"No:"+a);
+          }
+        }
+        System.out.println("Final Count:"+count);
+   }
+}
